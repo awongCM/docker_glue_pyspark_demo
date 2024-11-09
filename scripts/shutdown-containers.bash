31d5ce6b42eec5ee4e9d2b4f9545e67410675222
awongCM <awong.cm@gmail.com>
2024-11-09 23:15:10 +1100

    added scripts to start and stop containers

---
+++ scripts/shutdown-containers.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -ex  # Exit on error, print commands
+
+docker-compose down
---
+++ scripts/start-containers.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -ex  # Exit on error, print commands
+
+docker build 
+docker-compose up -d
